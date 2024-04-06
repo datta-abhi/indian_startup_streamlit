aad3abb6d44934f24dd1b60d319a108d8ae6bf37
datta-abhi <rookbee91@gmail.com>
2024-04-06 12:28:46 +0530

    investor list function modified to handle multiple investors in same round

--- data_analysis.py
+++ data_analysis.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
 
-# df = pd.read_csv('startup_funding.csv')
+# df = pd.read_csv('startup_cleaned.csv')
 
 def get_startup_list(df):
-    startup_list = df['Startup Name'].unique().tolist()
+    startup_list = sorted(df['Startup'].unique().tolist())
     return startup_list
 
 def get_investor_list(df):
-    investor_list = df['Investors Name'].unique().tolist()
+    investor_list = sorted(set(df['Investor'].str.split(',').sum()))
     return investor_list
+
